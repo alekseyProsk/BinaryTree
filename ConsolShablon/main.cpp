60542d4d63a75170546690fd096584f985337a4a
aregaerg <asdasdas@gmail.com>
2022-09-14 19:27:35 +0300

    добавлен вложенный класс Node/ добавлены поля и конструктор BinaryTree

--- ConsolShablon/main.cpp
+++ ConsolShablon/main.cpp
@@ -5,8 +5,25 @@ using namespace std;
 class BinarTree
 {
 public:
+	BinarTree() = default;
+	BinarTree(int data) 
+	{
+		root = new Node(data);
+	}
+	
 
 private:
+	class Node
+	{
+	public:
+		Node(int data) : data{ data }, left{ nullptr }, right{ nullptr }{}
+		Node* left;
+		Node* right;
+		int data;
+	
+	};
+	Node* root;
+
 };
 
 int main()
